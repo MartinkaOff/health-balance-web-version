5133fa77358259f43831801006d8da37c758dfee
Ildar Gazizov <110162487+MartinkaOff@users.noreply.github.com>
2024-08-20 21:53:56 +0500

    feat: add adaptive for 'Интересное' and fix title in 'Восстановление пароля'

--- src/pages/Access-recovery-page/Access-recovery-page.tsx
+++ src/pages/Access-recovery-page/Access-recovery-page.tsx
@@ -1,36 +1,36 @@
-import { useState } from 'react'
-import Header from '../../Components/Header/Header'
-import { AccessRecoveryItem } from '../../Components/Access-recovery/Access-recovery-item'
-import { stageAccessRecovery } from '../../utils/enums'
+import { useState } from "react";
+import Header from "../../Components/Header/Header";
+import { AccessRecoveryItem } from "../../Components/Access-recovery/Access-recovery-item";
+import { stageAccessRecovery } from "../../utils/enums";
 
 export const AccessRecoveryPage = () => {
-  const [order, setOrder] = useState<number>(0)
+	const [order, setOrder] = useState<number>(0);
 
-  const AccessRecoveryItems = () => {
-    switch (order) {
-      case 0:
-        return (
-          <AccessRecoveryItem
-            stage={stageAccessRecovery.email}
-            order={order}
-            setOrder={setOrder}
-          />
-        )
-      case 1:
-        return (
-          <AccessRecoveryItem
-            stage={stageAccessRecovery.password}
-            order={order}
-            setOrder={setOrder}
-          />
-        )
-    }
-  }
+	const AccessRecoveryItems = () => {
+		switch (order) {
+			case 0:
+				return (
+					<AccessRecoveryItem
+						stage={stageAccessRecovery.email}
+						order={order}
+						setOrder={setOrder}
+					/>
+				);
+			case 1:
+				return (
+					<AccessRecoveryItem
+						stage={stageAccessRecovery.password}
+						order={order}
+						setOrder={setOrder}
+					/>
+				);
+		}
+	};
 
-  return (
-    <div className={'access-recovery-page'}>
-      <Header title={'Восстановление пароля'} />
-      {AccessRecoveryItems()}
-    </div>
-  )
-}
+	return (
+		<div className={"access-recovery-page"}>
+			<Header title={"Восстановление пароля"} />
+			{AccessRecoveryItems()}
+		</div>
+	);
+};
